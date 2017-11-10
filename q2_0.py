3f40411d24b6409e8f6ad9ef1be6734d8388911f
stephen bai <stephenbai91@gmail.com>
2017-11-09 22:07:40 -0500

    finished 2.0

--- q2_0.py
+++ q2_0.py
@@ -14,11 +14,17 @@ def plot_means(train_data, train_labels):
     means = []
     for i in range(0, 10):
         i_digits = data.get_digits_by_label(train_data, train_labels, i)
-        # Compute mean of class i
+        # Compute mean of class i, num x 64
+        i_means = np.mean(i_digits, axis=0).reshape(8, 8)
+        # print(i_means.shape)
+        means.append(i_means)
 
     # Plot all means on same axis
     all_concat = np.concatenate(means, 1)
+    # 8x80, and 80 ==> horizontal expanding
+    # print(all_concat.shape)
     plt.imshow(all_concat, cmap='gray')
+    plt.title('10 means side by side graph')
     plt.show()
 
 if __name__ == '__main__':
