0d03e1ef5026c81b5b516838f9b3033c87bf261e
stephen bai <stephenbai91@gmail.com>
2017-11-12 19:17:09 -0500

    2.3 finish? needa check

--- q2_3.py
+++ q2_3.py
@@ -15,6 +15,7 @@ def binarize_data(pixel_values):
     '''
     return np.where(pixel_values > 0.5, 1.0, 0.0)
 
+
 def compute_parameters(train_data, train_labels):
     '''
     Compute the eta MAP estimate/MLE with augmented data
@@ -23,15 +24,31 @@ def compute_parameters(train_data, train_labels):
     where the ith row corresponds to the ith digit class.
     '''
     eta = np.zeros((10, 64))
+    # k means class type, j means feature dimension
+    for k in range(10):
+        # count number of datums of the k class
+        k_data = train_data[train_labels == k]
+
+        eta[k] = (np.sum(k_data, axis=0) + 1) / (k_data.shape[0] * 3)
+        print("the shape is: ", eta[k].shape)
+
+    print("eta shape is: ", eta.shape)
     return eta
+
 
 def plot_images(class_images):
     '''
-    Plot each of the images corresponding to each class side by side in grayscale
+    Plot each of the image eta corresponding to each class side by side in grayscale
     '''
+    img = []
     for i in range(10):
-        img_i = class_images[i]
-        # ...
+        img_i = class_images[i].reshape(8, 8)
+        img.append(img_i)
+
+    all_concat = np.concatenate(img, axis=1)
+    plt.imshow(all_concat, cmap='gray')
+    #plt.title('10 class eta side by side graph')
+    plt.show()
 
 def generate_new_data(eta):
     '''
@@ -41,7 +58,17 @@ def generate_new_data(eta):
     Plot these values
     '''
     generated_data = np.zeros((10, 64))
+    # bernoulli_rate = 0.9
+    bernoulli_rate = 0.5
+    s = np.random.binomial(1, bernoulli_rate, 64)
+    s_inverse = 1 - s
+
+    for k in range(10):
+        data_k = np.power( eta[k], s) * np.power(1-eta[k], s_inverse)
+        generated_data[k] = data_k
+
     plot_images(generated_data)
+
 
 def generative_likelihood(bin_digits, eta):
     '''
@@ -50,7 +77,35 @@ def generative_likelihood(bin_digits, eta):
 
     Should return an n x 10 numpy array 
     '''
-    return None
+    likelihood_set = generative_likelihood_probability(bin_digits, eta)
+    log_likelihood_set = np.log(likelihood_set)
+
+    return log_likelihood_set
+
+
+def generative_likelihood_probability(bin_digits, eta):
+    '''
+    Compute the generative likelihood:
+             p(x|y, eta)
+    Should return an n x 10 numpy array
+    '''
+
+    # eta: 10x64, bin_digits: nx64
+    # 10 classes
+
+    # Result ==> n x 10
+    n = bin_digits.shape[0]
+    likelihood_set = np.zeros((n, 10))
+    for i in range(n):
+        for k in range(10):
+            likelihood_v = np.power(eta[k], bin_digits[i]) * \
+                         np.power(1-eta[k], 1-bin_digits[i])
+
+            likelihood = np.product(likelihood_v)
+            likelihood_set[i][k] = likelihood
+
+    return likelihood_set
+
 
 def conditional_likelihood(bin_digits, eta):
     '''
@@ -61,7 +116,15 @@ def conditional_likelihood(bin_digits, eta):
     This should be a numpy array of shape (n, 10)
     Where n is the number of datapoints and 10 corresponds to each digit class
     '''
-    return None
+    # n x 10 ==>
+    generative_likelihood_set = generative_likelihood_probability(bin_digits, eta)
+    conditional_likelihood_probability = generative_likelihood_set\
+                                         /np.sum(generative_likelihood_set,
+                                                 axis=1, keepdims=True)
+
+    log_c_l_p = np.log(conditional_likelihood_probability)
+    return log_c_l_p
+
 
 def avg_conditional_likelihood(bin_digits, labels, eta):
     '''
@@ -71,10 +134,24 @@ def avg_conditional_likelihood(bin_digits, labels, eta):
 
     i.e. the average log likelihood that the model assigns to the correct class label
     '''
-    cond_likelihood = conditional_likelihood(bin_digits, eta)
+    cond_log_likelihood = conditional_likelihood(bin_digits, eta)
 
     # Compute as described above and return
-    return None
+    tot_probability = 0
+    for i in range(10):
+        # get conditional likelihood n x 10
+        # of each i and sum them up
+        matching_index = labels == i
+        cond_log_likelihood_i = cond_log_likelihood[matching_index]
+
+        cond_log_likelihood_i_max = np.max(cond_log_likelihood_i, axis=1)
+        i_tot = np.sum(cond_log_likelihood_i_max)
+        tot_probability += i_tot
+
+    # Compute as described above and return
+    # over all digits
+    return tot_probability/bin_digits.shape[0]
+
 
 def classify_data(bin_digits, eta):
     '''
@@ -82,19 +159,53 @@ def classify_data(bin_digits, eta):
     '''
     cond_likelihood = conditional_likelihood(bin_digits, eta)
     # Compute and return the most likely class
-    pass
+
+    cond_likelihood = conditional_likelihood(bin_digits, eta)
+
+    posterior_class_prediction = np.argmax(cond_likelihood, axis=1)
+    return posterior_class_prediction
+
+
+def posterior_accuracy(bin_digits, labels, eta):
+    posterior_class_prediction = classify_data(bin_digits, eta)
+    correct_count = posterior_class_prediction[posterior_class_prediction
+                                               == labels].shape[0]
+    accuracy = correct_count / bin_digits.shape[0]
+    # print("posterior accuracy is: ", accuracy)
+    return accuracy
 
 def main():
     train_data, train_labels, test_data, test_labels = data.load_all_data('data')
     train_data, test_data = binarize_data(train_data), binarize_data(test_data)
 
     # Fit the model
+    # 2.3.2
     eta = compute_parameters(train_data, train_labels)
 
-    # Evaluation
-    plot_images(eta)
+    # Evaluation 2.3.3
+    # plot_images(eta)
 
-    generate_new_data(eta)
+    # generate_new_data(eta)
+
+    avg_cond_likelihood = avg_conditional_likelihood(train_data,
+                                                     train_labels, eta)
+
+    print("avg_cond_likelihood train is:{}, probability: {} ".format(avg_cond_likelihood,
+                                                 np.exp(avg_cond_likelihood)))
+
+    avg_cond_likelihood = avg_conditional_likelihood(test_data,
+                                                     test_labels, eta)
+
+    print("avg_cond_likelihood test is:{}, probability: {} ".format(avg_cond_likelihood,
+                                                       np.exp(avg_cond_likelihood)))
+
+    # test posterior class accuracy
+    train_posterior_accuracy = posterior_accuracy(train_data, train_labels, eta)
+    print("train posterior_accuracy is: ", train_posterior_accuracy)
+
+    test_posterior_accuracy = posterior_accuracy(test_data, test_labels, eta)
+    print("test posterior_accuracy is: ", test_posterior_accuracy)
+
 
 if __name__ == '__main__':
     main()
